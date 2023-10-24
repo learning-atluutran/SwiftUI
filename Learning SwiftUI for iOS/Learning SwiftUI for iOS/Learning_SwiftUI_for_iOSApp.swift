0f1912a306be7120973a6e5bdac63529f341ccb5
An Luu <atluutran@gmail.com>
2023-10-24 09:05:25 -0400

    Renamed folder: 'Learning SwiftUI for iOS' to iOS

---
+++ Learning SwiftUI for iOS/Learning SwiftUI for iOS/Learning_SwiftUI_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUI_for_iOSApp.swift
+//  Learning SwiftUI for iOS
+//
+//  Created by An Luu on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUI_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
