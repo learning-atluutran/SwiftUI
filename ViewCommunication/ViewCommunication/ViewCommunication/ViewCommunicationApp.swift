450bcc1cf65acc215f6be9340b003c68650d47f5
atluutran <atluutran@gmail.com>
2023-11-02 14:24:57 -0400

    Added proj 'ViewCommunication'

---
+++ ViewCommunication/ViewCommunication/ViewCommunication/ViewCommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewCommunicationApp.swift
+//  ViewCommunication
+//
+//  Created by An Luutran on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewCommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
